586102419b1a1842560213a83fb036b1cc882d06
georgehahn <hahnerg@gmail.com>
2023-06-07 14:50:59 -0400

    Yo i changed the data yo

--- Graphing data.py
+++ Graphing data.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import json
-import numpy as np
 
-FILE = "data/BA_2023-05-29 18:15:04.787691.json"
+URL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=1min&outputsize=full&adjusted=false&apikey=PJNT54CJJ4A2CTA8"
+
+FILE = "data/BA_2023-06-07 14:39:38.824716.json"
 
 file = open(FILE)
 
@@ -15,8 +16,10 @@ time_series = data["Time Series (1min)"]
 for key in time_series.keys():
     stock_price.append(float(time_series[key]["4. close"]))
 
-
 fig, ax = plt.subplots()
 ax.plot(stock_price)
+plt.title("Stock Prices")
+plt.xlabel("Minute of Day")
+plt.ylabel("Stock Price")
 plt.show()
 
